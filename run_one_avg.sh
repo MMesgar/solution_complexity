7914de931ab73882bb09bcd78b1c2893dc03e899
Mohsen Mesgar <mohsen.mesgar@h-its.org>
2017-06-10 09:19:18 +0200

    run the model on all corpora

--- run_one_avg.sh
+++ run_one_avg.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# theano device
-device=gpu0
-
 # model parameters
 MODE='-nonstatic'
 WRD2VEC='-word2vec' # -word2vec -rand
@@ -11,10 +8,46 @@ BATCH_SIZE=32
 DROPOUT_RATE=0.5
 max_sl=600
 
+# corpus 2-3
 exp_id='one_avg_2_3'
 OUT_DIR='./evalutions/'$exp_id'/'
 IN_DIR='./data/corpus_2_3/'
 
 mkdir -p $OUT_DIR
 
-THEANO_FLAGS=device=$device,floatX=float32,lib.cnmem=0.7 python -u model_one_avg.py $MODE $WRD2VEC $N_EPOCHS $BATCH_SIZE  $DROPOUT_RATE $IN_DIR $OUT_DIR $max_sl
+THEANO_FLAGS=device=gpu0,floatX=float32,lib.cnmem=0.7 python -u model_one_avg.py 
+$MODE $WRD2VEC $N_EPOCHS $BATCH_SIZE  $DROPOUT_RATE $IN_DIR $OUT_DIR $max_sl > 
+./script_out/'$exp_id.log
+
+# corpus 4-5
+exp_id='one_avg_4_5'
+OUT_DIR='./evalutions/'$exp_id'/'
+IN_DIR='./data/corpus_4_5/'
+
+mkdir -p $OUT_DIR
+
+THEANO_FLAGS=device=gpu1,floatX=float32,lib.cnmem=0.7 python -u model_one_avg.py 
+$MODE $WRD2VEC $N_EPOCHS $BATCH_SIZE  $DROPOUT_RATE $IN_DIR $OUT_DIR $max_sl > 
+./script_out/'$exp_id.log
+
+# corpus gr-5
+exp_id='one_avg_gr_5'
+OUT_DIR='./evalutions/'$exp_id'/'
+IN_DIR='./data/corpus_gr_5/'
+
+mkdir -p $OUT_DIR
+
+THEANO_FLAGS=device=gpu1,floatX=float32,lib.cnmem=0.7 python -u model_one_avg.py 
+$MODE $WRD2VEC $N_EPOCHS $BATCH_SIZE  $DROPOUT_RATE $IN_DIR $OUT_DIR $max_sl > 
+./script_out/'$exp_id.log
+
+# corpus all
+exp_id='one_avg_all'
+OUT_DIR='./evalutions/'$exp_id'/'
+IN_DIR='./data/corpus_all/'
+
+mkdir -p $OUT_DIR
+
+THEANO_FLAGS=device=gpu1,floatX=float32,lib.cnmem=0.7 python -u model_one_avg.py 
+$MODE $WRD2VEC $N_EPOCHS $BATCH_SIZE  $DROPOUT_RATE $IN_DIR $OUT_DIR $max_sl > 
+./script_out/'$exp_id.log
