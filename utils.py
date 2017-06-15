0da7ab0ea0caad8d8fe91735fcb95567f0354f56
Mohsen Mesgar <mohsen.mesgar@h-its.org>
2017-06-15 19:00:09 +0200

    add rnn

--- utils.py
+++ utils.py
@@ -187,3 +187,50 @@ def as_floatX(variable):
     if isinstance(variable, np.ndarray):
         return np.cast[theano.config.floatX](variable)
     return theano.tensor.cast(variable, theano.config.floatX)
+#%%
+def uniform(rng, shape, scale=0.08):
+    return np.asarray(rng.uniform(
+                    low=-scale, high=scale,
+                    size=shape),
+                    dtype=theano.config.floatX)
+#%%
+def zero(rng, shape):
+    return np.asarray(np.zeros(shape),
+                      dtype=theano.config.floatX)
+
+#%%
+def one(rng, shape):
+    return np.asarray(np.ones(shape),
+                      dtype=theano.config.floatX)
+
+#%%
+def eye(rng, rows):
+    return np.asarray(np.eye(rows),
+                      dtype=theano.config.floatX)
+
+#%%
+def orthogonal(rng, shape, scale=1.1, name=None):
+    # From Lasagne. Reference: Saxe et al., http://arxiv.org/abs/1312.6120
+    flat_shape = (shape[0], np.prod(shape[1:]))
+    a = rng.normal(0.0, 1.0, flat_shape)
+    u, _, v = np.linalg.svd(a, full_matrices=False)
+    # pick the one with the correct shape
+    q = u if u.shape == flat_shape else v
+    q = q.reshape(shape)
+    return scale * q[:shape[0], :shape[1]]
+
+#%%
+def get(rng, identifier, shape):
+
+    if identifier == 'uniform':
+        return uniform(rng,shape)
+    elif identifier == 'orthonormal':
+        return np.asarray(orthogonal(rng,shape), dtype=theano.config.floatX)
+    elif identifier == 'zero':
+        return zero(rng,shape)
+    elif identifier == 'one':
+        return one(rng,shape)
+    elif identifier == 'eye':
+        return eye(rng,shape[0])
+    else:
+        raise NotImplementedError
